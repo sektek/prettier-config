81b69687f6aafaf06378834e040c1fd0f5c08ae0
Edward Kelly <eddie@sektek.net>
2024-04-19 13:58:44 +0000

    chore: updated semantic-release config

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,21 @@
  */
 module.exports = {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        draftRelease: true,
+      },
+    ],
+    '@semantic-release/git',
+  ],
 };
